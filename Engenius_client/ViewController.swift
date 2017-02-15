7e83483f20b1a5cdbbe7d310e25d6a1e23cfe84b
ishikawa-pro <ishikawa.pro@gmail.com>
2017-02-16 00:33:57 +0900

    Initial Commit

---
+++ Engenius_client/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Engenius_client
+//
+//  Created by 石川諒 on 2017/02/16.
+//  Copyright © 2017年 石川諒. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
