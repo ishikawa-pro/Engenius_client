6c4aa0e31934185db63df79083727bbcd9d0e93b
ishikawa-pro <ishikawa.pro@gmail.com>
2017-03-26 02:35:20 +0900

    [add]ViewControllerへHttp_helperを追加
    記事を取得するためのHttp_helperを追加。
    Http_helperからNotificationCenterを使って記事の取得通知を受け取り、got_articlesで処理を行う

--- Engenius_client/ViewController.swift
+++ Engenius_client/ViewController.swift
@@ -8,18 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    //記事の取得用クラスHttp_helperのインスタンスの生成
+    let http_helper = Http_helper.init(base_url: "http://localhost:3000/article.json")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //記事の取得通知を受け取るように登録
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(self.got_articles),
+            name: NSNotification.Name("got_articles"),
+            object: nil
+        )
+        //記事を取得
+        self.http_helper.get_articles(params: ["category":"Docker","limit":"3"])
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    //記事を取得したら呼ばれる
+    func got_articles() -> Void {
+        for data in self.http_helper.articles {
+            if let title = data["title"] as? String {
+                print(title)
+            }
+        }
+    }
 
 }
 
