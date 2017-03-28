9911f1b693e34cef6a48feb5db64f1e26f0b51a4
ishikawa-pro <ishikawa.pro@gmail.com>
2017-03-28 17:14:37 +0900

    [add] 記事一覧用TableViewのカスタムセルのxibファイルとクラスを追加
    記事の一覧用のカスタムセルを作成。
    とりあえずTitleとサムネイルを表示するようにtitleLabelとthumbnailImageView
    をセル内に追加

---
+++ Engenius_client/ArticlesTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  ArticlesTableViewCell.swift
+//  Engenius_client
+//
+//  Created by 石川諒 on 2017/03/27.
+//  Copyright © 2017年 石川諒. All rights reserved.
+//
+
+import UIKit
+
+class ArticlesTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
