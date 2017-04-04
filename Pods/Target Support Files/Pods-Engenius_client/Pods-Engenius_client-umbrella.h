7fec24b6357995512fbb906727833a5ec378b613
ishikawa-pro <ishikawa.pro@gmail.com>
2017-04-04 18:41:28 +0900

    [add]cocoaPods関係のファイルを追加

---
+++ Pods/Target Support Files/Pods-Engenius_client/Pods-Engenius_client-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Engenius_clientVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Engenius_clientVersionString[];
+
