40f8357551522513f73b0736d905e6f3b37bea61
ishikawa-pro <ishikawa.pro@gmail.com>
2017-11-11 09:39:31 +0900

    categoryのエンドポイントのURLRouterを作成

--- Engenius_client/EngeniusAPIRouter.swift
+++ Engenius_client/EngeniusAPIRouter.swift
@@ -11,4 +11,21 @@ import Alamofire
 
 enum EngeniusAPIRouter {
     static let baseURLString = "engeniusalb-2015328251.ap-northeast-1.elb.amazonaws.com"
+
+    enum category : URLRequestConvertible {
+        case getCategories()
+
+        var path: String {
+            switch self {
+            case .getCategories:
+                return "/category.json"
+            }
+        }
+
+        func asURLRequest() throws -> URLRequest {
+            let url = try EngeniusAPIRouter.baseURLString.asURL()
+            let urlRequest = URLRequest(url: url.appendingPathComponent(path))
+            return try URLEncoding.default.encode(urlRequest, with: nil)
+        }
+    }
 }
