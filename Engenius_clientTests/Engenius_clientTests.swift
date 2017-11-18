9e7711a27317f09fe81638238e655d3235dda850
ishikawa-pro <ishikawa.pro@gmail.com>
2017-11-18 11:08:09 +0900

    不要なコメントの削除

--- Engenius_clientTests/Engenius_clientTests.swift
+++ Engenius_clientTests/Engenius_clientTests.swift
@@ -91,7 +91,6 @@ class Engenius_clientTests: XCTestCase {
         let baseURL = "http://engeniusalb-2015328251.ap-northeast-1.elb.amazonaws.com/article"
 
         var request = URLRequest(url: URL(string: baseURL + ".json?limit=7")!)
-        //XCTAssertEqual(try! EngeniusAPIRouter.article.getFeed(limit: 7,page: 0).asURLRequest(), request)
         XCTAssertEqual(try! EngeniusAPIRouter.article.fetchFeed(limit: 7, page: 0).asURLRequest(), request)
 
         request = URLRequest(url: URL(string: baseURL + ".json?limit=7&offset=7")!)
