d7a4609fe3dcfdf79218a3ab7890c914204485ad
石川諒 <ishikawa.pro@gmail.com>
2017-11-30 01:11:03 +0900

    add ConfigViewController

---
+++ Engenius_client/ConfigViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ConfigViewController.swift
+//  Engenius_client
+//
+//  Created by 石川諒 on 2017/11/29.
+//  Copyright © 2017年 石川諒. All rights reserved.
+//
+
+import UIKit
+
+class ConfigViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
