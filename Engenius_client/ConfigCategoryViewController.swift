c6b624c5c5c55b996f8a9b4c9a41c2fa21c3e940
石川諒 <ishikawa.pro@gmail.com>
2017-11-30 17:21:25 +0900

    セクション数を１個に設定

--- Engenius_client/ConfigCategoryViewController.swift
+++ Engenius_client/ConfigCategoryViewController.swift
@@ -35,7 +35,7 @@ class ConfigCategoryViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
