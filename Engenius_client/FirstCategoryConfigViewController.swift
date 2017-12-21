9499569df0b59afdaf6c6b195f911cac47e9bd02
石川諒 <ishikawa.pro@gmail.com>
2017-12-21 22:40:40 +0900

    add FirstCategoryConfigViewController

---
+++ Engenius_client/FirstCategoryConfigViewController.swift
@@ -0,0 +1,15 @@
+//
+//  FirstCategoryConfigViewController.swift
+//  Engenius_client
+//
+//  Created by 石川諒 on 2017/12/21.
+//  Copyright © 2017年 石川諒. All rights reserved.
+//
+
+import Foundation
+
+class FirstCategorySelectViewController: ConfigCategoryViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
