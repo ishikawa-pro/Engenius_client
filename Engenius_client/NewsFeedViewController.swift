41736189e758de8e1768ab4262817abfdee93f32
石川諒 <ishikawa.pro@gmail.com>
2018-01-12 10:07:16 +0900

    CategoryArticlesViewControllerを元にNewsFeedViewControllerを作成

--- Engenius_client/NewsFeedViewController.swift
+++ Engenius_client/NewsFeedViewController.swift
@@ -7,29 +7,159 @@
 //
 
 import UIKit
+import XLPagerTabStrip
 
-class NewsFeedViewController: UIViewController {
+class NewsFeedViewController: UIViewController, ArticlesViewController {
+
+    var delegate: ArticlesViewControllerDelegate!
+    var engeniusAPIClient: EngeniusAPIClient = EngeniusAPIClient(apiClient: AlamofireClient())
+    //ページ数
+    //オフセット数は、EngeniusAPIRouterで設定
+    private var page = 0
+    //記事を取りに行っている否か
+    var isFetching = false
+    //記事の格納用
+    //tableViewの宣言
+    var articleTableView = UITableView()
+    var articles: [Article] = [] {
+        didSet {
+            //記事を追加読み込みする場合はreloadData
+            //初めてTableViewを描画するときはaddSubview
+            if self.articles.count > EngeniusAPIRouter.article.limit {
+                self.isFetching = false
+                articleTableView.reloadData()
+            } else {
+                //記事を取得できた時点でtableViewを表示させる
+                view.addSubview(articleTableView)
+            }
+
+            return
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
 
-        // Do any additional setup after loading the view.
+        //tableViewの作成
+        articleTableView = UITableView(frame: view.frame)
+        //tableViewのデリゲートを設定
+        articleTableView.delegate = self
+        //tableViewのデーターソースを設定
+        articleTableView.dataSource = self
+        articleTableView.prefetchDataSource = self
+
+        // カスタムセルクラス名でnibを作成する
+        let nib = UINib(nibName: "ArticlesTableViewCell", bundle: nil)
+        articleTableView.register(nib, forCellReuseIdentifier: "customCell")
+        fetchArticles()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if isFetching {
+            //request?.resume()
+            engeniusAPIClient.apiClient.resume()
+        }
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        if isFetching {
+            //request?.suspend()
+            engeniusAPIClient.apiClient.suspend()
+        }
+    }
+
+    func fetchArticles() {
+        engeniusAPIClient.fetchNewsFeed(categories: ["Swift", "Docker"], page: page) { response in
+            //記事がなければappendせずにreturn
+            if response.count == 0 {
+                //tableの終端でisFetchingをtrueにすることで新しい記事を取りに行けなくする。
+                self.isFetching = true
+                return
+            } else {
+                self.articles.append(contentsOf: response)
+                self.page += 1
+            }
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
-    // MARK: - Navigation
+    //スクロールするたびに呼ばれる
+    //スクロールが早すぎて、セルのpreFetchが行われない場合は、スクロールの終端を判定して新しい記事を取りに行く
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        //contentOffset.y + frame.size.height = UITableViewの高さ0からの変位量 + TableViewの高さ
+        //contentSize.height = スクロールする中身の高さ
+        //isFetching = 記事を取りに行っているか
+        //UITableViewの高さ0からの変位量 + TableViewの高さ > スクロールする中身の高さ
+        //スクロール中かどうか
+        if articleTableView.contentOffset.y + articleTableView.frame.size.height >
+            articleTableView.contentSize.height &&
+            articleTableView.isDragging && !isFetching {
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+            fetchArticles()
+            isFetching = true
+
+            //一番下まで行った時に全てのtableViewで残り50pointだけスクロールできない問題の暫定処置
+            articleTableView.contentSize = CGSize.init(width: articleTableView.contentSize.width, height: articleTableView.contentSize.height + 145)
+        }
     }
-    */
+}
+
+extension NewsFeedViewController : UITableViewDelegate {
+    //セルがタップされたら呼ばれる
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //webViewControllerへ遷移する部分をデリゲートする。
+        delegate.showArticle(url: articles[indexPath.row].url)
+    }
+}
+
+extension NewsFeedViewController : UITableViewDataSource {
+    //各行に表示するcellを定義
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        //cellの作成
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as? ArticlesTableViewCell {
+            return setArticleCell(cell: cell, article: articles[indexPath.row])
+        }
+        return setArticleCell(article: articles[indexPath.row])
+    }
+
+    //cellの数を指定
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        //記事の数に応じたcell数を返す
+        return articles.count
+    }
 
 }
+
+extension NewsFeedViewController : UITableViewDataSourcePrefetching {
+    func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
+        guard let row = indexPaths.last?.row else {
+            return
+        }
+        //row + 1 はcellの数
+        if row + 1 == articles.count && !isFetching {
+            fetchArticles()
+            isFetching = true
+            //最後のcellが50pt分だけスクロールできない問題の暫定措置
+            articleTableView.contentSize = CGSize.init(width: articleTableView.contentSize.width, height: articleTableView.contentSize.height + 145)
+        }
+    }
+}
+
+extension NewsFeedViewController : IndicatorInfoProvider {
+    func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
+        if let barTitle = title {
+            return IndicatorInfo(title: barTitle)
+        } else {
+            return IndicatorInfo(title: "No title")
+        }
+    }
+}
+
