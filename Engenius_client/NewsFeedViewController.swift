cb6844570827f45c533401e60fad8b5e2857bd50
石川諒 <ishikawa.pro@gmail.com>
2018-01-12 10:06:22 +0900

    add NewsFeedViewController

---
+++ Engenius_client/NewsFeedViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NewsFeedViewController.swift
+//  Engenius_client
+//
+//  Created by 石川諒 on 2018/01/12.
+//  Copyright © 2018年 石川諒. All rights reserved.
+//
+
+import UIKit
+
+class NewsFeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
