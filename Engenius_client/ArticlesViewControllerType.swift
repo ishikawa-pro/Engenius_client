a37922e8d26076d60f28e6e1e8197cacbb68772c
石川諒 <ishikawa.pro@gmail.com>
2018-02-22 09:38:55 +0900

    親Viewに書くべき関数はprotocolから親Viewへ移動して、APIのレスポンスのclosureをprocoolへ追加

--- Engenius_client/ArticlesViewControllerType.swift
+++ Engenius_client/ArticlesViewControllerType.swift
@@ -15,40 +15,34 @@ protocol ArticlesViewControllerDelegate {
     func showArticle(url: URL?)
 }
 
-
 protocol ArticlesViewControllerType {
     var indicatorTitle: String? { get set }
     var delegate: ArticlesViewControllerDelegate? { get set }
-    func fetchArticles()
-    func downloadThumbnail(imageURL: URL, imageView: UIImageView)
-    func setArticleCell(cell: ArticlesTableViewCell, article: Article ) -> ArticlesTableViewCell
+    var articles: [Article] {get set}
+    var isFetching: Bool {get set}
+    var notificationCenter: NotificationCenter? {get set}
+    var response: (([Article]) -> ()) {get}
+    var articleTableView: UITableView? {get set}
+    func fetchArticles()    
 }
 
-extension ArticlesViewControllerType where Self: UIViewController {
-    func downloadThumbnail(imageURL: URL, imageView: UIImageView) {
-        //2回目以降キャッシュが使われる
-        imageView.af_setImage(withURL: imageURL) { (response) in
-            switch (response.result) {
-            case .success(let result):
-                imageView.image = result
-            case .failure(let error):
-                print(error)
+extension ArticlesViewControllerType where Self: ArticlesViewController {
+    var response: (([Article]) -> ()) {
+        get {
+            return {response in
+                //記事がなければappendせずにreturn
+                if response.count == 0 {
+                    //tableの終端でisFetchingをtrueにすることで新しい記事を取りに行けなくする。
+                    self.isFetching = true
+                    return
+                } else {
+                    self.articles.append(contentsOf: response)
+                    self.page += 1
+                }
             }
         }
     }
-
-    func setArticleCell(cell: ArticlesTableViewCell = ArticlesTableViewCell(), article: Article ) -> ArticlesTableViewCell {
-        //再利用するcellの画像残っているので、デフォルトの画像に一旦差し替える。
-        cell.thumbnailImageView.image =  UIImage(named: "81v2Ahk8X-L._SX355_.jpg")
-        cell.titleLabel.text = article.title
-        guard let url = article.imageURL else {
-            return cell
-        }
-        downloadThumbnail(imageURL: url, imageView: cell.thumbnailImageView)
-        return cell
-    }
 }
-
 
 extension IndicatorInfoProvider where Self: ArticlesViewController {
     func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
