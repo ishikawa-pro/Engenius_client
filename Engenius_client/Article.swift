84718311ace3b3954d69cbaa63a14b4f4ec3922a
石川諒 <ishikawa.pro@gmail.com>
2018-03-03 21:42:20 +0900

    サーバー側のレスポンスデータの変更を反映

--- Engenius_client/Article.swift
+++ Engenius_client/Article.swift
@@ -17,7 +17,7 @@ struct Article : Codable {
 
     enum CodingKeys: String, CodingKey {
         case title
-        case url = "link"
+        case url
         case postDate = "post_date"
         case imageURL = "image_url"
         case category
